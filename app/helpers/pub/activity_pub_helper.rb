396a7fdc610c9b23d9aff439466e2ea0868abad0
Alexa Kirchner <akirchner@leavemealone>
2022-12-11 00:34:33 -0600

    In depth signature logging

--- app/helpers/pub/activity_pub_helper.rb
+++ app/helpers/pub/activity_pub_helper.rb
@@ -22,24 +22,28 @@ module Pub
 		end
 
 		def sig_check(req_headers)
-			return false unless req_headers['Signature']
+			if !req_headers['Signature']
+				p "Signature failed due to lack of signature header"
+				return false
+			end
 
 			sig_header = req_headers['Signature'].split(',').map do |pair|
 				parts = pair.match(/(.*)=\"(.*)\"/)
 				[parts[1], parts[2]]
 			end.to_h
-			p "sig_header: #{sig_header}"
 
 			key_id = sig_header['keyId']
 			headers = sig_header['headers']
 			signature = Base64.decode64(sig_header['signature'])
-			p "Signature: #{signature}"
 
 			actor_response = HTTP.headers(
-				'Content-Type' => 'application/json',
+				'Content-Type' => 'application/activity+json',
 				'Accept': 'application/json'
 			).get(key_id)
-			return false unless actor_response.code == 200
+			if actor_response.code != 200
+				p "Signature failed due to not getting the actor properly"
+				return false
+			end
 
 			actor = JSON.parse(actor_response.to_s)
 			key = OpenSSL::PKey::RSA.new(actor['publicKey']['publicKeyPem'])
@@ -51,14 +55,28 @@ module Pub
 					"#{header_name}: #{req_headers[header_name.capitalize]}"
 				end
 			end.join("\n")
-			p comparison_string
 
 			date = DateTime.parse(req_headers['Date'])
-			p "Date: #{date}"
-			# Check the digest
-			# Check that the actor and who's following match
-			key.verify(OpenSSL::Digest::SHA256.new, signature, comparison_string) &&
-				date > 1.minute.ago
+			if !key.verify(OpenSSL::Digest::SHA256.new, signature, comparison_string) 
+				p "Signature failed due to verify"
+				p "-------------- Signature ---------------------"
+				p req_headers['Signature']
+				p "------------- header signature ---------------"
+				p sig_header['signature']
+				p "------------- signature ----------------------"
+				p signature
+				p "------------ comparison string ---------------"
+				p comparison_string
+				p "-----------------------------------------------"
+				return false;
+			end
+				
+			if date < 1.minute.ago
+				p "Signature failed due to the date"
+				return false
+			end
+
+			return true
 		end
 	end
 end
